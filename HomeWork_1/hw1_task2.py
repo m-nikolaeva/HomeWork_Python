7452f6b61846de7120a0a79018235f21818ef72e
Marina <mv.nikolaeva1507@gmail.com>
2022-09-28 10:35:08 +0300

    correct

--- HomeWork_1/hw1_task2.py
+++ HomeWork_1/hw1_task2.py
@@ -31,3 +31,16 @@ else:
 #         for z in range(2):
 #             if not (x or y or z) == (not x and not y and not z):
 #                 print(x, y, z)
+
+
+# =============================================================
+i = 0
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (not (x or y or z) == (not x and not y and not z)):
+                i += 1
+if i:
+    print('False')
+else:
+    print('All True')
--- HomeWork_1/hw1_task5.py
+++ HomeWork_1/hw1_task5.py
@@ -13,3 +13,23 @@ yb = int(input('y = '))
 
 distance = ((xa - xb) ** 2 + (ya - yb) ** 2) ** 0.5
 print(f'A({xa},{ya}); B({xb},{yb}) -> {round(distance, 2)}')
+
+# =============================================================
+
+x = float(input('Enter X: '))
+y = float(input('Enter Y: '))
+z = float(input('Enter Z: '))
+
+a = not(x or y or z)
+b = not x and not y and not z
+
+print(a == b)
+
+
+#============================
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == (not x and not y and not z):
+                print(x, y, z)
