92c1e75a15b14be4460c70429607deefdb34ac14
Marina <mv.nikolaeva1507@gmail.com>
2022-10-20 12:35:32 +0300

    correct user_interface.py

--- HomeWork_7/user_interface.py
+++ HomeWork_7/user_interface.py
@@ -1,33 +1,3 @@
-# from input_data import add_contact
-# from contacts_op import read_contacts, find_contact
-
-
-# def user_interface():
-#     flag = True
-#     while flag:
-#         print('\nДобро пожаловать в базу данных!\n\nВыберите пункт меню для продолжения: ')
-#         while True:
-#             print('1 - Найти контакт')
-#             print('2 - Добавить контакт')
-#             print('3 - Показать все контакты')
-#             print('4 - Выход')
-#             choice = input()
-#             if choice not in ['1', '2', '3', '4']:
-#                 print('Такого пункта нет. Попробуйте еще раз')
-#                 continue
-#             if choice == '1':
-#                 find_contact()
-#                 break
-#             elif choice == '2':
-#                 add_contact()
-#                 break
-#             elif choice == '3':
-#                 read_contacts()
-#             else:
-#                 flag = False
-#                 break
-            
-
 from input_data import add_contact
 from contacts_op import find_contact, read_contacts, write_contacts, get_columns
 
